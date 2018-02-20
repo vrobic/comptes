0fd1c62538fb05cab422751cd02e27844707f5e0
Lucile Daguise <ldaguise@umanit.fr>
2018-02-20 13:49:51 +0100

    Installation de PHPSpreadSheet, le successeur de PHPExcel et adaptation des handlers Excel.

--- src/ComptesBundle/Service/ImportHandler/CMExcelMouvementsImportHandler.php
+++ src/ComptesBundle/Service/ImportHandler/CMExcelMouvementsImportHandler.php
@@ -2,8 +2,11 @@
 
 namespace ComptesBundle\Service\ImportHandler;
 
-use Ddeboer\DataImport\Reader\ExcelReader;
+use PhpOffice\PhpSpreadsheet\Reader\Exception;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use ComptesBundle\Entity\Mouvement;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
  * Implémente un handler Excel d'import de mouvements de la banque Crédit Mutuel.
@@ -14,6 +17,9 @@ class CMExcelMouvementsImportHandler extends AbstractMouvementsImportHandler
      * Parse les mouvements et remplit les tableaux de classification du handler.
      *
      * @param \SplFileObject $file Fichier Excel fourni par le Crédit Mutuel.
+     *
+     * @throws \Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
      */
     public function parse(\SplFileObject $file)
     {
@@ -23,48 +29,81 @@ class CMExcelMouvementsImportHandler extends AbstractMouvementsImportHandler
         // Configuration du handler
         $configuration = $this->configuration['cm.excel']['config'];
 
-        // Tableau de correspondance entre l'index de la feuille et le compte bancaire
+        // Attention : ceci est susceptible de changer car la banque met parfois à
+        // jour ses formats de fichiers.
+        $startRowNumber = 6;
+        $dateColumnId = "A";
+        $descriptionColumnId = "C";
+        $debitColumnId = "D";
+        $creditColumnId = "E";
+
+        // Tableau de correspondance entre l'index de la feuille et le compte
+        // bancaire.
         $comptesBySheets = array();
 
         foreach ($configuration['sheets'] as $sheetIndex => $compteID) {
             $comptesBySheets[$sheetIndex] = $compteRepository->find($compteID);
         }
 
+        // Lecture des cellules et création des mouvements bancaires.
         foreach ($comptesBySheets as $sheetIndex => $compte) {
+            try {
+                $reader = IOFactory::load($file->getRealPath());
+                $worksheet = $reader->getSheet($sheetIndex);
+                $rowIterator = $worksheet->getRowIterator($startRowNumber);
 
-            $reader = new ExcelReader($file, 4, $sheetIndex);
+                foreach ($rowIterator as $row) {
+                    $index = $row->getRowIndex();
 
-            foreach ($reader as $row) {
+                    // Récupération des cellules voulues.
+                    $debitCell = $worksheet->getCell($debitColumnId . $index);
+                    $creditCell = $worksheet->getCell($creditColumnId . $index);
+                    $descriptionCell = $worksheet->getCell(
+                        $descriptionColumnId . $index
+                    );
+                    $dateCell = $worksheet->getCell($dateColumnId . $index);
 
-                // Arrivée à la fin du tableau des mouvements
-                if ($row["Solde"] === null) {
-                    break;
+                    // Récupération des valeurs saisies dans les cellules.
+                    $debit = $debitCell->getValue();
+                    $credit = $creditCell->getValue();
+                    $description = $descriptionCell->getValue();
+                    $date = $dateCell->getValue();
+
+                    // Arrivée à la fin du tableau des mouvements
+                    if (null === $debit && null === $credit) {
+                        break;
+                    }
+
+                    $mouvement = new Mouvement();
+
+                    // Date
+                    $mouvement->setDate(Date::excelToDateTimeObject($date));
+
+                    // Compte
+                    $mouvement->setCompte($compte);
+
+                    // Montant
+                    $montant = $debit !== null ? $debit : $credit;
+
+                    $montant = sprintf('%0.2f', $montant);
+
+                    $mouvement->setMontant($montant);
+
+                    // Description
+                    $mouvement->setDescription($description);
+
+                    // Classification
+                    $classification = $this->getClassification($mouvement);
+                    $this->classify($mouvement, $classification);
                 }
-
-                $mouvement = new Mouvement();
-
-                // Date, Excel la stocke comme un integer. 0 = 01/01/1900, 25569 = 01/01/1970
-                $date = new \DateTime();
-                $daysSince1970 = $row["Date"] - 25569;
-                $timestamp = strtotime("+$daysSince1970 days", 0);
-                $date->setTimestamp($timestamp);
-                $mouvement->setDate($date);
-
-                // Compte
-                $mouvement->setCompte($compte);
-
-                // Montant
-                $montant = $row["Débit"] !== null ? $row["Débit"] : $row["Crédit"];
-                $montant = sprintf('%0.2f', $montant);
-                $mouvement->setMontant($montant);
-
-                // Description
-                $description = $row["Libellé"];
-                $mouvement->setDescription($description);
-
-                // Classification
-                $classification = $this->getClassification($mouvement);
-                $this->classify($mouvement, $classification);
+            } catch (Exception $e) {
+                break;
+                $session = new Session();
+                $session->start();
+                $session->getFlashBag()->add(
+                    'error',
+                    $e->getMessage()
+                );
             }
         }
     }
