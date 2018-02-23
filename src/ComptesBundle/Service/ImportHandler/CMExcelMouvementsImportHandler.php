07cc87e5e5dc7fc0f198edc54dada1290592bbe4
vrobic <vrobic@umanit.fr>
2018-02-23 10:30:17 +0100

    Ajustements suite à pull request #3

--- src/ComptesBundle/Service/ImportHandler/CMExcelMouvementsImportHandler.php
+++ src/ComptesBundle/Service/ImportHandler/CMExcelMouvementsImportHandler.php
@@ -2,24 +2,24 @@
 
 namespace ComptesBundle\Service\ImportHandler;
 
-use PhpOffice\PhpSpreadsheet\Reader\Exception;
-use PhpOffice\PhpSpreadsheet\Shared\Date;
-use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet;
 use ComptesBundle\Entity\Mouvement;
-use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
  * Implémente un handler Excel d'import de mouvements de la banque Crédit Mutuel.
  */
 class CMExcelMouvementsImportHandler extends AbstractMouvementsImportHandler
 {
+    const START_ROW_NUMBER = 6;
+    const DATE_COLUMN_ID = 'A';
+    const DESCRIPTION_COLUMN_ID = 'C';
+    const DEBIT_COLUMN_ID = 'D';
+    const CREDIT_COLUMN_ID = 'E';
+
     /**
      * Parse les mouvements et remplit les tableaux de classification du handler.
      *
      * @param \SplFileObject $file Fichier Excel fourni par le Crédit Mutuel.
-     *
-     * @throws \Exception
-     * @throws \PhpOffice\PhpSpreadsheet\Exception
      */
     public function parse(\SplFileObject $file)
     {
@@ -29,81 +29,52 @@ class CMExcelMouvementsImportHandler extends AbstractMouvementsImportHandler
         // Configuration du handler
         $configuration = $this->configuration['cm.excel']['config'];
 
-        // Attention : ceci est susceptible de changer car la banque met parfois à
-        // jour ses formats de fichiers.
-        $startRowNumber = 6;
-        $dateColumnId = "A";
-        $descriptionColumnId = "C";
-        $debitColumnId = "D";
-        $creditColumnId = "E";
-
-        // Tableau de correspondance entre l'index de la feuille et le compte
-        // bancaire.
+        // Tableau de correspondance entre l'index de la feuille et le compte bancaire
         $comptesBySheets = array();
 
         foreach ($configuration['sheets'] as $sheetIndex => $compteID) {
             $comptesBySheets[$sheetIndex] = $compteRepository->find($compteID);
         }
 
-        // Lecture des cellules et création des mouvements bancaires.
+        $reader = PhpSpreadsheet\IOFactory::load($file->getRealPath());
+
         foreach ($comptesBySheets as $sheetIndex => $compte) {
-            try {
-                $reader = IOFactory::load($file->getRealPath());
-                $worksheet = $reader->getSheet($sheetIndex);
-                $rowIterator = $worksheet->getRowIterator($startRowNumber);
 
-                foreach ($rowIterator as $row) {
-                    $index = $row->getRowIndex();
+            $sheet = $reader->getSheet($sheetIndex);
+            $rowIterator = $sheet->getRowIterator(self::START_ROW_NUMBER);
 
-                    // Récupération des cellules voulues.
-                    $debitCell = $worksheet->getCell($debitColumnId . $index);
-                    $creditCell = $worksheet->getCell($creditColumnId . $index);
-                    $descriptionCell = $worksheet->getCell(
-                        $descriptionColumnId . $index
-                    );
-                    $dateCell = $worksheet->getCell($dateColumnId . $index);
+            foreach ($rowIterator as $row) {
 
-                    // Récupération des valeurs saisies dans les cellules.
-                    $debit = $debitCell->getValue();
-                    $credit = $creditCell->getValue();
-                    $description = $descriptionCell->getValue();
-                    $date = $dateCell->getValue();
+                $rowIndex = $row->getRowIndex();
+                $date = $sheet->getCell(self::DATE_COLUMN_ID . $rowIndex)->getValue();
+                $debit = $sheet->getCell(self::DEBIT_COLUMN_ID . $rowIndex)->getValue();
+                $credit = $sheet->getCell(self::CREDIT_COLUMN_ID . $rowIndex)->getValue();
+                $description = $sheet->getCell(self::DESCRIPTION_COLUMN_ID . $rowIndex)->getValue();
 
-                    // Arrivée à la fin du tableau des mouvements
-                    if (null === $debit && null === $credit) {
-                        break;
-                    }
-
-                    $mouvement = new Mouvement();
-
-                    // Date
-                    $mouvement->setDate(Date::excelToDateTimeObject($date));
-
-                    // Compte
-                    $mouvement->setCompte($compte);
-
-                    // Montant
-                    $montant = $debit !== null ? $debit : $credit;
-
-                    $montant = sprintf('%0.2f', $montant);
-
-                    $mouvement->setMontant($montant);
-
-                    // Description
-                    $mouvement->setDescription($description);
-
-                    // Classification
-                    $classification = $this->getClassification($mouvement);
-                    $this->classify($mouvement, $classification);
+                // Arrivée à la fin du tableau des mouvements
+                if (null === $debit && null === $credit) {
+                    break;
                 }
-            } catch (Exception $e) {
-                break;
-                $session = new Session();
-                $session->start();
-                $session->getFlashBag()->add(
-                    'error',
-                    $e->getMessage()
-                );
+
+                $mouvement = new Mouvement();
+
+                // Date
+                $mouvement->setDate(PhpSpreadsheet\Shared\Date::excelToDateTimeObject($date));
+
+                // Compte
+                $mouvement->setCompte($compte);
+
+                // Montant
+                $montant = $debit !== null ? $debit : $credit;
+                $montant = sprintf('%0.2f', $montant);
+                $mouvement->setMontant($montant);
+
+                // Description
+                $mouvement->setDescription($description);
+
+                // Classification
+                $classification = $this->getClassification($mouvement);
+                $this->classify($mouvement, $classification);
             }
         }
     }
