20fabb378a00bd02ce0d0b6e40aaee83cd712ebb
vrobic <robicvincent@gmail.com>
2026-01-23 21:24:44 +0100

    ♻️ le contrôleur d'import des mouvements n'a plus de gros switch case mais trois méthodes bien distinctes

--- src/Infrastructure/Controller/MouvementsImportController.php
+++ src/Infrastructure/Controller/MouvementsImportController.php
@@ -28,9 +28,6 @@ use Symfony\Component\Routing\Attribute\Route;
 /**
  * Contrôleur du centre d'import.
  *
- * @todo Faire une fonction pour chaque action :
- *       - mouvementsParseAction
- *       - mouvementsImportAction
  * @todo Utiliser un service pour mutualiser le reste.
  */
 class MouvementsImportController extends AbstractController
@@ -59,191 +56,191 @@ class MouvementsImportController extends AbstractController
     }
 
     /**
-     * Contrôleur d'import des mouvements. Se déroule en trois étapes :
-     *      - parsing du fichier uploadé
-     *      - affichage d'un formulaire permettant d'ajuster les données
-     *      - validation et import
-     *
-     * @todo Comme pour MouvementController->edit(), utiliser un formulaire Symfony.
+     * Affiche le formulaire d'import des mouvements.
+     */
+    #[Route('/import/mouvements', name: 'import_mouvements', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render(
+            'Import/mouvements.html.twig',
+            [
+                'handlers' => $this->handlers,
+                'categorized_mouvements' => new MouvementCollection(),
+                'uncategorized_mouvements' => new MouvementCollection(),
+                'ambiguous_mouvements' => new MouvementCollection(),
+                'waiting_mouvements' => new MouvementCollection(),
+            ]
+        );
+    }
+
+    /**
+     * Parse le fichier uploadé et affiche les mouvements à importer.
      *
      * @throws \Exception en cas d'erreur d'import du fichier
      */
-    #[Route('/import/mouvements', name: 'import_mouvements')]
-    public function __invoke(Request $request): Response
+    #[Route('/import/mouvements/parse', name: 'import_mouvements_parse', methods: ['POST'])]
+    public function parse(Request $request): Response
     {
-        // Classification des mouvements
-        $categorizedMouvements = new MouvementCollection();
-        $uncategorizedMouvements = new MouvementCollection();
-        $ambiguousMouvements = new MouvementCollection();
-        $waitingMouvements = new MouvementCollection();
+        /** @var MouvementsImportHandlerInterface $handler */
+        $handler = $this->getHandler($request);
+        $splFile = $this->getFile($request);
+        $mouvementsParClassification = $handler->parse($splFile);
 
-        switch ($request->get('action')) {
-            case null:
-                return $this->render(
-                    'Import/mouvements.html.twig',
-                    [
-                        'handlers' => $this->handlers,
-                        'categorized_mouvements' => $categorizedMouvements,
-                        'uncategorized_mouvements' => $uncategorizedMouvements,
-                        'ambiguous_mouvements' => $ambiguousMouvements,
-                        'waiting_mouvements' => $waitingMouvements,
-                    ]
-                );
+        $mouvements = $mouvementsParClassification->getMouvements();
 
-            case 'parse': // Parsing du fichier uploadé
-                /** @var MouvementsImportHandlerInterface $handler */
-                $handler = $this->getHandler($request);
-                $splFile = $this->getFile($request);
-                $mouvementsParClassification = $handler->parse($splFile);
+        /** @var MouvementCollection $categorizedMouvements */
+        $categorizedMouvements = $mouvementsParClassification->has(Classification::CATEGORIZED) ?
+            $mouvementsParClassification->get(Classification::CATEGORIZED) :
+            new MouvementCollection();
 
-                $mouvements = $mouvementsParClassification->getMouvements();
+        /** @var MouvementCollection $uncategorizedMouvements */
+        $uncategorizedMouvements = $mouvementsParClassification->has(Classification::UNCATEGORIZED) ?
+            $mouvementsParClassification->get(Classification::UNCATEGORIZED) :
+            new MouvementCollection();
 
-                /** @var MouvementCollection $categorizedMouvements */
-                $categorizedMouvements = $mouvementsParClassification->has(Classification::CATEGORIZED) ?
-                    $mouvementsParClassification->get(Classification::CATEGORIZED) :
-                    new MouvementCollection();
+        /** @var MouvementCollection $ambiguousMouvements */
+        $ambiguousMouvements = $mouvementsParClassification->has(Classification::AMBIGUOUS) ?
+            $mouvementsParClassification->get(Classification::AMBIGUOUS) :
+            new MouvementCollection();
 
-                /** @var MouvementCollection $uncategorizedMouvements */
-                $uncategorizedMouvements = $mouvementsParClassification->has(Classification::UNCATEGORIZED) ?
-                    $mouvementsParClassification->get(Classification::UNCATEGORIZED) :
-                    new MouvementCollection();
+        /** @var MouvementCollection $waitingMouvements */
+        $waitingMouvements = $mouvementsParClassification->has(Classification::WAITING) ?
+            $mouvementsParClassification->get(Classification::WAITING) :
+            new MouvementCollection();
 
-                /** @var MouvementCollection $ambiguousMouvements */
-                $ambiguousMouvements = $mouvementsParClassification->has(Classification::AMBIGUOUS) ?
-                    $mouvementsParClassification->get(Classification::AMBIGUOUS) :
-                    new MouvementCollection();
+        // Indique si on doit ignorer les mouvements anciens
+        $skipOldOnes = $request->get('skipOldOnes', false);
 
-                /** @var MouvementCollection $waitingMouvements */
-                $waitingMouvements = $mouvementsParClassification->has(Classification::WAITING) ?
-                    $mouvementsParClassification->get(Classification::WAITING) :
-                    new MouvementCollection();
+        // Le dernier mouvement inséré
+        $latestMouvement = $this->mouvementRepository->findLatestOne();
 
-                // Indique si on doit ignorer les mouvements anciens
-                $skipOldOnes = $request->get('skipOldOnes', false);
+        // Passage des mouvements en session
+        foreach ($mouvements as $mouvement) {
+            /* Si on doit ignorer les mouvements anciens,
+             * alors on n'importe le mouvement que s'il est plus récent que le dernier présent en base. */
+            if (false !== $skipOldOnes && $latestMouvement instanceof Mouvement) {
+                $date = $mouvement->date;
+                $latestMouvementDate = $latestMouvement->date;
 
-                // Le dernier mouvement inséré
-                $latestMouvement = $this->mouvementRepository->findLatestOne();
+                if ($date < $latestMouvementDate) {
+                    $mouvements = $mouvements->remove($mouvement);
+                    $categorizedMouvements = $categorizedMouvements->remove($mouvement);
+                    $uncategorizedMouvements = $uncategorizedMouvements->remove($mouvement);
+                    $ambiguousMouvements = $ambiguousMouvements->remove($mouvement);
+                    $waitingMouvements = $waitingMouvements->remove($mouvement);
 
-                // Passage des mouvements en session
-                foreach ($mouvements as $mouvement) {
-                    /* Si on doit ignorer les mouvements anciens,
-                     * alors on n'importe le mouvement que s'il est plus récent que le dernier présent en base. */
-                    if (false !== $skipOldOnes && $latestMouvement instanceof Mouvement) {
-                        $date = $mouvement->date;
-                        $latestMouvementDate = $latestMouvement->date;
-
-                        if ($date < $latestMouvementDate) {
-                            $mouvements = $mouvements->remove($mouvement);
-                            $categorizedMouvements = $categorizedMouvements->remove($mouvement);
-                            $uncategorizedMouvements = $uncategorizedMouvements->remove($mouvement);
-                            $ambiguousMouvements = $ambiguousMouvements->remove($mouvement);
-                            $waitingMouvements = $waitingMouvements->remove($mouvement);
-
-                            continue;
-                        }
-                    }
+                    continue;
                 }
-
-                $request->getSession()->set('mouvements', $mouvements);
-
-                return $this->render(
-                    'Import/mouvements.html.twig',
-                    [
-                        'handlers' => $this->handlers,
-                        'categorized_mouvements' => $categorizedMouvements,
-                        'uncategorized_mouvements' => $uncategorizedMouvements,
-                        'ambiguous_mouvements' => $ambiguousMouvements,
-                        'waiting_mouvements' => $waitingMouvements,
-                        'comptes' => $this->compteRepository->findAll(),
-                        'categories' => $this->categorieRepository->findAll()->toAssociativeArray(),
-                    ]
-                );
-
-            case 'import': // Import des mouvements après ajustements manuels
-                // Identifiants des mouvements à importer
-                $idsToImport = $request->get('ids_to_import', []);
-
-                // Données de mouvements à modifier
-                $mouvementsData = $request->get('mouvements', []);
-
-                /**
-                 * Récupération des mouvements.
-                 *
-                 * @var Mouvement[] $mouvements
-                 */
-                $mouvements = $request->getSession()->get('mouvements');
-
-                foreach ($mouvements as $mouvement) {
-                    $id = (string) $mouvement->id;
-
-                    if (!in_array($id, $idsToImport)) {
-                        continue;
-                    }
-
-                    // Modification éventuelle de la date
-                    if (isset($mouvementsData[$id]['date'])) {
-                        $dateString = $mouvementsData[$id]['date'];
-                        $date = \DateTimeImmutable::createFromFormat('d-m-Y H:i:s', "$dateString 00:00:00");
-                        if (!($date instanceof \DateTimeImmutable)) {
-                            throw new BadRequestHttpException("Date du mouvement invalide : $dateString");
-                        }
-                        $mouvement->date = $date;
-                    }
-
-                    // Modification éventuelle de la catégorie
-                    if (isset($mouvementsData[$id]['categorie'])) {
-                        $categorieId = CategorieId::estValide((string) $mouvementsData[$id]['categorie']) ?
-                            new CategorieId((string) $mouvementsData[$id]['categorie']) :
-                            null;
-
-                        if ($categorieId instanceof CategorieId) {
-                            $categorie = $this->categorieRepository->find($categorieId);
-
-                            if (!($categorie instanceof Categorie)) {
-                                throw new BadRequestHttpException("Catégorie $categorieId introuvable");
-                            }
-                        } else {
-                            $categorie = null;
-                        }
-
-                        $mouvement->categorie = $categorie;
-                    }
-
-                    // Modification éventuelle du compte
-                    if (isset($mouvementsData[$id]['compte'])) {
-                        if (CompteId::estValide((string) $mouvementsData[$id]['compte'])) {
-                            $compteId = new CompteId((string) $mouvementsData[$id]['compte']);
-                            $compte = $this->compteRepository->find($compteId);
-
-                            if (!($compte instanceof Compte)) {
-                                throw new BadRequestHttpException("Compte $compteId introuvable");
-                            }
-
-                            $mouvement->compte = $compte;
-                        }
-                    }
-
-                    // Modification éventuelle du montant
-                    if (isset($mouvementsData[$id]['montant'])) {
-                        $montant = new Montant((float) $mouvementsData[$id]['montant']);
-                        $mouvement->montant = $montant;
-                    }
-
-                    // Modification éventuelle de la description
-                    if (isset($mouvementsData[$id]['description'])) {
-                        $description = $mouvementsData[$id]['description'];
-                        $mouvement->description = $description;
-                    }
-
-                    // Persistance des données
-                    $this->mouvementRepository->save($mouvement); // @todo : sortir de la boucle
-                }
-
-                // Vidage de la session
-                $request->getSession()->remove('mouvements');
-
-                return $this->redirectToRoute('import_mouvements');
+            }
         }
+
+        $request->getSession()->set('mouvements', $mouvements);
+
+        return $this->render(
+            'Import/mouvements.html.twig',
+            [
+                'handlers' => $this->handlers,
+                'categorized_mouvements' => $categorizedMouvements,
+                'uncategorized_mouvements' => $uncategorizedMouvements,
+                'ambiguous_mouvements' => $ambiguousMouvements,
+                'waiting_mouvements' => $waitingMouvements,
+                'comptes' => $this->compteRepository->findAll(),
+                'categories' => $this->categorieRepository->findAll()->toAssociativeArray(),
+            ]
+        );
+    }
+
+    /**
+     * Importe les mouvements après ajustements manuels.
+     *
+     * @throws BadRequestHttpException en cas d'erreur de validation
+     */
+    #[Route('/import/mouvements/import', name: 'import_mouvements_import', methods: ['POST'])]
+    public function import(Request $request): Response
+    {
+        // Identifiants des mouvements à importer
+        $idsToImport = $request->get('ids_to_import', []);
+
+        // Données de mouvements à modifier
+        $mouvementsData = $request->get('mouvements', []);
+
+        /**
+         * Récupération des mouvements.
+         *
+         * @var Mouvement[] $mouvements
+         */
+        $mouvements = $request->getSession()->get('mouvements');
+
+        foreach ($mouvements as $mouvement) {
+            $id = (string) $mouvement->id;
+
+            if (!in_array($id, $idsToImport)) {
+                continue;
+            }
+
+            // Modification éventuelle de la date
+            if (isset($mouvementsData[$id]['date'])) {
+                $dateString = $mouvementsData[$id]['date'];
+                $date = \DateTimeImmutable::createFromFormat('d-m-Y H:i:s', "$dateString 00:00:00");
+                if (!($date instanceof \DateTimeImmutable)) {
+                    throw new BadRequestHttpException("Date du mouvement invalide : $dateString");
+                }
+                $mouvement->date = $date;
+            }
+
+            // Modification éventuelle de la catégorie
+            if (isset($mouvementsData[$id]['categorie'])) {
+                $categorieId = CategorieId::estValide((string) $mouvementsData[$id]['categorie']) ?
+                    new CategorieId((string) $mouvementsData[$id]['categorie']) :
+                    null;
+
+                if ($categorieId instanceof CategorieId) {
+                    $categorie = $this->categorieRepository->find($categorieId);
+
+                    if (!($categorie instanceof Categorie)) {
+                        throw new BadRequestHttpException("Catégorie $categorieId introuvable");
+                    }
+                } else {
+                    $categorie = null;
+                }
+
+                $mouvement->categorie = $categorie;
+            }
+
+            // Modification éventuelle du compte
+            if (isset($mouvementsData[$id]['compte'])) {
+                if (CompteId::estValide((string) $mouvementsData[$id]['compte'])) {
+                    $compteId = new CompteId((string) $mouvementsData[$id]['compte']);
+                    $compte = $this->compteRepository->find($compteId);
+
+                    if (!($compte instanceof Compte)) {
+                        throw new BadRequestHttpException("Compte $compteId introuvable");
+                    }
+
+                    $mouvement->compte = $compte;
+                }
+            }
+
+            // Modification éventuelle du montant
+            if (isset($mouvementsData[$id]['montant'])) {
+                $montant = new Montant((float) $mouvementsData[$id]['montant']);
+                $mouvement->montant = $montant;
+            }
+
+            // Modification éventuelle de la description
+            if (isset($mouvementsData[$id]['description'])) {
+                $description = $mouvementsData[$id]['description'];
+                $mouvement->description = $description;
+            }
+
+            // Persistance des données
+            $this->mouvementRepository->save($mouvement); // @todo : sortir de la boucle
+        }
+
+        // Vidage de la session
+        $request->getSession()->remove('mouvements');
+
+        return $this->redirectToRoute('import_mouvements');
     }
 
     /**
